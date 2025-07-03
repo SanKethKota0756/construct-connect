59f0f405d21721aa9736101665d5876da24b0145
Sanketh Kota <sanketh056@gmail.com>
2025-07-03 19:46:37 +0530

    Fix: Add missing CareersPage component

---
+++ frontend/src/pages/info/CareersPage.jsx
@@ -0,0 +1,15 @@
+// frontend/src/pages/info/CareersPage.jsx
+
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const CareersPage = () => {
+  return (
+    <Container className="py-5">
+      <h1>Careers at ConstructConnect</h1>
+      <p>This page is under construction. Information about job openings will be here soon!</p>
+    </Container>
+  );
+};
+
+export default CareersPage;
